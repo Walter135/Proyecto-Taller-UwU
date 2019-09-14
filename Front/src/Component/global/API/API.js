7d4c8df4299833fecaf076ad5d793993b946ef6a
Walter Rivas <walter.13569@gmail.com>
2019-09-14 11:46:20 -0500

    primer commit

---
+++ Front/src/Component/global/API/API.js
@@ -0,0 +1,6 @@
+export default {
+    //"url":" https://modulocontrol-sigap.herokuapp.com/"
+    //"url": "https://stackers-back.herokuapp.com/"
+    //"url": "http://localhost:7896/"
+    "url": "https://control-recibo-uwu-back.herokuapp.com/"
+}
