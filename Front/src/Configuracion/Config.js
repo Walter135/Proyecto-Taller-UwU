a45efd80d4eb7db20e56f3b5e1695b24f0119fc4
Walter Rivas <walter.13569@gmail.com>
2019-09-18 21:38:24 -0500

    Actualizacion

--- Front/src/Configuracion/Config.js
+++ Front/src/Configuracion/Config.js
@@ -3,5 +3,5 @@
    //   const CONFIG= 'https://tcsbackf.herokuapp.com/'
    // const CONFIG= 'https://sigadev1-consultarecibos-back.herokuapp.com/' 
    //const CONFIG= 'http://localhost:8080/';
-   //Rorro Kbrazooooooooooooooooooo
+   //Rorro Kbrito
 export default CONFIG;
