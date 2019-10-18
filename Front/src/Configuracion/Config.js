2c7a4b90fef430f3d0f14574e1d8d105ab7f1f1f
Walter Rivas <walter.13569@gmail.com>
2019-10-18 12:14:57 -0500

    Funcionalidad

--- Front/src/Configuracion/Config.js
+++ Front/src/Configuracion/Config.js
@@ -1,7 +1,7 @@
 
-   // const CONFIG= 'https://uwu-back.herokuapp.com/'
+   const CONFIG= 'https://uwu-back.herokuapp.com/'
    //   const CONFIG= 'https://tcsbackf.herokuapp.com/'
    // const CONFIG= 'https://sigadev1-consultarecibos-back.herokuapp.com/' 
-   const CONFIG= 'http://localhost:8080/';
+   // const CONFIG= 'http://localhost:8080/';
    //Rorro Kbrito
 export default CONFIG;
